d2caf53d6a78916253e8289caed0d4b0eb5477e0
Eric Muller <EricMuller22@gmail.com>
2014-01-14 15:05:39 -0800

    Pull map screenshot from SQLite db

--- HaloTimer/Model/HTMapListDataSource.m
+++ HaloTimer/Model/HTMapListDataSource.m
@@ -55,22 +55,32 @@
             NSLog(@"failed to open map database");
         }
         
-        NSString *query = @"SELECT name, sniper, rocket, shotgun, camo, overshield FROM map ORDER BY name ASC";
+        NSString *query = @"SELECT name, sniper, rocket, shotgun, camo, overshield, image FROM map ORDER BY name ASC";
         sqlite3_stmt *statement;
         if (sqlite3_prepare_v2(mapDatabase, [query UTF8String], -1, &statement, nil) == SQLITE_OK) {
             while (sqlite3_step(statement) == SQLITE_ROW) {
+                // Map name and timing info
                 NSString *mapName = [[NSString alloc] initWithUTF8String:(char *)sqlite3_column_text(statement, 0)];
                 NSInteger sniper = sqlite3_column_int(statement, 1);
                 NSInteger rocket = sqlite3_column_int(statement, 2);
                 NSInteger shotgun = sqlite3_column_int(statement, 3);
                 NSInteger camo = sqlite3_column_int(statement, 4);
                 NSInteger overshield = sqlite3_column_int(statement, 5);
+                
                 HTMap *map = [HTMap mapWithName:mapName
                                          rocket:rocket
                                          sniper:sniper
                                         shotgun:shotgun
                                            camo:camo
                                      overshield:overshield];
+                
+                // Map screenshot
+                int length = sqlite3_column_bytes(statement, 6);
+                NSData *imageData = [NSData dataWithBytes:sqlite3_column_blob(statement, 6) length:length];
+                if(imageData != nil) {
+                    map.screenshot = [UIImage imageWithData:imageData];
+                }
+                
                 [mapArray addObject:map];
             }
             sqlite3_finalize(statement);
